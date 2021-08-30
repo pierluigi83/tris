3f82b11141417dd592845c1dae557327a9c180f9
pierluigi83 <ildragoneblu@libero.it>
2021-08-30 09:38:23 +0200

    split code

--- tris.js
+++ tris.js
@@ -7,28 +7,6 @@ giocatore[0].cell = new Array();
 giocatore[1].cell = new Array();
 var i = 0;
 var v = false;
-function gioco(id){
-	if (v)
-		alert('Gioco finito');
-	else{
-		element = document.getElementById(id);
-		if (giocatore[0].turn)
-			i = 0;
-		else 
-			i = 1;
-		cell = giocatore[i].cell;
-		if (element.innerHTML == ''){ 
-			element.innerHTML = giocatore[i].sign;
-			cell.push(id);
-			giocatore[i].turn = false;
-			giocatore[Math.abs(i-1)].turn = true;
-			if (cell.length >= 3)
-				check_tris();
-			if ((cell.length == 5) && !v)
-				alert('Non ha vinto nessuno');
-		}
-	}
-}
 function check_tris(){
 	var idvect = giocatore[i].cell;
 	if ((/1/.test(idvect) && /2/.test(idvect) && /3/.test(idvect))
@@ -42,6 +20,4 @@ function check_tris(){
 		setTimeout(function() {alert ('hai vinto!')},200);
 		v = true;
 	}
-
-
 }
